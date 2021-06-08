921ea21d35339129ed865d467d515f61c0856226
Anthony Petrides <anthony.petrides@thetrainline.com>
2021-06-08 08:48:47 +0100

    update webpack version

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,27 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './src/index.js',
-  externals: [nodeExternals()],
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: '',
-    libraryTarget: 'commonjs'
-  },
-  plugins: [new CleanWebpackPlugin()],
-  module: {
-    rules: [
-     {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      { 
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, './src')
-      }
-    ]
-  }
+	entry: './src/index.js',
+	externals: [nodeExternals()],
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'dist'),
+		library: '',
+		libraryTarget: 'commonjs'
+	},
+	plugins: [new CleanWebpackPlugin()],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+				include: path.resolve(__dirname, './src')
+			}
+		]
+	}
 }
